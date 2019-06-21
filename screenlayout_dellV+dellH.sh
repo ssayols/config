91a5d0854c23398fe195b404454623e9dfb67fbf
ssayols <sergisayolspuig@gmail.com>
2019-06-21 13:07:01 +0200

    niu screen layout

--- screenlayout_dellV+dellH.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-if $(xrandr | grep -q "HDMI3 connected") && $(xrandr | grep -q "VGA1 connected"); then
-    xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --mode 1920x1200 --pos 0x0 --rotate left --output HDMI2 --off --output HDMI1 --off --output LVDS1 --off --output VGA1 --primary --mode 1920x1200 --pos 1200x304 --rotate normal
-fi
