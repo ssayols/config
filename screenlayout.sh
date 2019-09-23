4e160c911039c593a6e7e986d450c56ae32a1dda
ssayols <sergisayolspuig@gmail.com>
2019-09-23 11:08:58 +0200

    new screen port at niu

--- screenlayout.sh
+++ screenlayout.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # NIU with small 19" external + laptop + docking station
-if $(xrandr | grep -q "^eDP1 connected") && $(xrandr | grep -q "^DP2-1 connected .*1440x900"); then
-  xrandr --output DP2-1 --primary --mode 1440x900 --pos 0x0 --rotate normal --output eDP1 --mode 1920x1080 --pos 1440x0 --rotate normal
+if $(xrandr | grep -q "^eDP1 connected") && $(xrandr | grep -q "^DP1 connected .*1440x900"); then
+  xrandr --output DP1 --primary --mode 1440x900 --pos 0x0 --rotate normal --output eDP1 --mode 1920x1080 --pos 1440x0 --rotate normal
 # IMB with 24" external + laptop
 # HDMI1 connected 1920x1200+0+0 (normal left inverted right x axis y axis) 520mm x 320mm
 elif $(xrandr | grep -q "^eDP1 connected") && $(xrandr | grep -q "^HDMI1 connected .*1920x1200"); then
