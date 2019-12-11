0bdbd828d342dc5841658546546772d0353c319d
ssayols <sergisayolspuig@gmail.com>
2019-12-11 11:43:55 +0100

    now 43" screen at imb

--- screenlayout.sh
+++ screenlayout.sh
@@ -7,6 +7,10 @@ if $(xrandr | grep -q "^eDP1 connected") && $(xrandr | grep -q "^DP1 connected .
 # HDMI1 connected 1920x1200+0+0 (normal left inverted right x axis y axis) 520mm x 320mm
 elif $(xrandr | grep -q "^eDP1 connected") && $(xrandr | grep -q "^HDMI1 connected .*1920x1200"); then
   xrandr --output HDMI1 --primary --mode 1920x1200 --pos 0x0 --rotate normal --output eDP1 --mode 1920x1080 --pos 1920x0 --rotate normal
+# IMB with 43" external + laptop + docking station
+# DP1-2 connected 3840x1200+0+0
+elif $(xrandr | grep -q "^eDP1 connected") && $(xrandr | grep -q "^DP1-2 connected .*3840x1200"); then
+  xrandr --output DP1-2 --primary --mode 3840x1200 --refresh 120 --pos 0x0 --rotate normal --output eDP1 --off
 # home with 35" external + laptop + docking station
 elif $(xrandr | grep -q "^eDP1 connected") && $(xrandr | grep -q "^DP2-1 connected .*3440x1440"); then
   xrandr --output DP2-1 --primary --mode 3440x1440 --refresh 100 --pos 0x0 --rotate normal --output eDP1 --off
