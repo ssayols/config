973de6f06e1ccae2c64feb9a079ec20edc93ee52
ssayols <sergisayolspuig@gmail.com>
2022-01-18 15:21:29 +0100

    change screenlayout at home

--- screenlayout.sh
+++ screenlayout.sh
@@ -1,24 +1,11 @@
 #!/bin/sh
 
-# NIU with small 24" external + laptop + docking station
-if $(xrandr | grep -q "^eDP1 connected") && $(xrandr | grep -q "^DP1 connected .*1920x1200"); then
-  xrandr --output DP1 --primary --mode 1920x1200 --pos 0x0 --rotate normal --output eDP1 --mode 1920x1080 --pos 1920x0 --rotate normal
+# home with 35" external (right, primary, set to --auto refresh (60hz) instead of --refresh 100) + laptop (left) + docking station
+if $(xrandr | grep -q "^eDP1 connected") && $(xrandr | grep -q "^DP2-1 connected .*3440x1440"); then
+  DP2_res=$(xrandr | grep -A1 "^DP2-1 connected ." | tail -n +2 | sed 's/^\s\+\([0-9x]\+\).\+/\1/')
+  xrandr --output DP2-1 --primary --mode $DP2_res --auto --pos 1920x0 --rotate normal --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal
 
-# IMB with 24" external + laptop
-# HDMI1 connected 1920x1200+0+0 (normal left inverted right x axis y axis) 520mm x 320mm
-elif $(xrandr | grep -q "^eDP1 connected") && $(xrandr | grep -q "^HDMI1 connected .*1920x1200"); then
-  xrandr --output HDMI1 --primary --mode 1920x1200 --pos 0x0 --rotate normal --output eDP1 --mode 1920x1080 --pos 1920x0 --rotate normal
-
-# IMB with 43" external + laptop + docking station
-# DP1-2 connected 3840x1200+0+0
-elif $(xrandr | grep -q "^eDP1 connected") && $(xrandr | grep -q "^DP1-2 connected .*3840x1200"); then
-  xrandr --output DP1-2 --primary --mode 3840x1200 --refresh 120 --pos 0x0 --rotate normal --output eDP1 --off
-
-# home with 35" external + laptop + docking station
-elif $(xrandr | grep -q "^eDP1 connected") && $(xrandr | grep -q "^DP2-1 connected .*3440x1440"); then
-  xrandr --output DP2-1 --primary --mode 3440x1440 --refresh 100 --pos 0x0 --rotate normal --output eDP1 --off
-
-# laptop only: redefine dpi size for high dpi (ubuntu defaults to 96)
+# laptop only: redefine dpi size for high dpi (ubuntu defaults to 96, auto should set it to something ~166dpi)
 elif $(xrandr | grep -q "^eDP1 connected"); then
-  xrandr --output eDP1 #--dpi 166
+  xrandr --output eDP1 # --auto --dpi 166
 fi
