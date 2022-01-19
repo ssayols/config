7450225a96f50f49c220ec322db9562550f36029
ssayols <sergisayolspuig@gmail.com>
2022-01-19 11:29:18 +0100

    screen layout should run on connected but not active devices also

--- screenlayout.sh
+++ screenlayout.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # home with 35" external (right, primary, set to --auto refresh (60hz) instead of --refresh 100) + laptop (left) + docking station
-if $(xrandr | grep -q "^eDP1 connected") && $(xrandr | grep -q "^DP2-1 connected .*3440x1440"); then
+if $(xrandr | grep -q "^eDP1 connected") && $(xrandr | grep -q "^DP2-1 connected"); then
   DP2_res=$(xrandr | grep -A1 "^DP2-1 connected ." | tail -n +2 | sed 's/^\s\+\([0-9x]\+\).\+/\1/')
   xrandr --output DP2-1 --primary --mode $DP2_res --auto --pos 1920x0 --rotate normal --output eDP1 --mode 1920x1080 --pos 0x0 --rotate normal
 
