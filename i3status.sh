4fca3d5f4b0749ec2d09516e883db14093fc4668
ssayols <ssayols@users.noreply.github.com>
2024-07-16 17:13:48 +0200

    simplify language option in i3status.sh

--- i3status.sh
+++ i3status.sh
@@ -4,10 +4,6 @@ i3status --config ~/.config/i3status/config | while :
 do
   read line
   LG=$(setxkbmap -query | awk '/layout/{print $2}') 
-  if [ $LG == "br" ]; then
-    dat="[{ \"full_text\": \"LANG: $LG\", \"color\":\"#009E00\" },"
-  else
-    dat="[{ \"full_text\": \"LANG: $LG\", \"color\":\"#C60101\" },"
-  fi
+  dat="[{ \"full_text\": \"LANG: $LG\", \"color\":\"#FFFFFF\" },"
   echo "${line/[/$dat}" || exit 1
 done
